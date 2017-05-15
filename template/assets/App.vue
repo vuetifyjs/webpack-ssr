94cc233a93046ecd43b9f2cf935ffdd598fae1e4
John Leider <jleider@fastforwardacademy.com>
2017-05-15 13:24:55 -0400

    tweaks

--- template/assets/App.vue
+++ template/assets/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-navigation-drawer
       persistent
-      :mini-variant="mini"
+      :mini-variant="miniVariant"
       :clipped="clipped"
       v-model="drawer"
     >
@@ -16,7 +16,7 @@
             :to="item.to"
           >
             <v-list-tile-action>
-              <v-icon light>{{ item.icon }}</v-icon>
+              <v-icon light v-html="item.icon"></v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
               <v-list-tile-title v-text="item.title"></v-list-tile-title>
@@ -29,9 +29,9 @@
       <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-btn 
         icon
-        @click.native.stop="mini = !mini"
+        @click.native.stop="miniVariant = !miniVariant"
       >
-        <v-icon>{{ mini ? 'chevron_right' : 'chevron_left' }}</v-icon>
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
       <v-btn
         icon
@@ -100,7 +100,7 @@
           { icon: 'apps', title: 'Welcome', to: '/' },
           { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
         ],
-        mini: false,
+        miniVariant: false,
         right: true,
         rightDrawer: false,
         title: 'Vuetify.js'
